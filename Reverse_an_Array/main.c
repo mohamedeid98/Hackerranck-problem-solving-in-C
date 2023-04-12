b9b0a26bbbffcdf335b19a0e6f94277820fc6553
Mohamed Eid <m0hamed.3ed98@gmail.com>
2023-04-12 03:19:32 +0200

    Extend the code to allow revrse from start to end

--- Reverse_an_Array/main.c
+++ Reverse_an_Array/main.c
@@ -11,12 +11,12 @@ void swap(int *a, int *b)
 }
 
 
-void reverse_array(int arr[], int size)
+void reverse_array(int arr[], int start, int end)
 {
-    int end = size - 1 ;
-    for (int i = 0 ; i < end ; i++, end--)
+    while(start < end)
     {
-        swap(&arr[i], &arr[end]);
+        swap(&arr[start], &arr[end]);
+        start++ ; end--;
     }
 
 }
@@ -36,7 +36,7 @@ int main()
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
     printArray(arr, n);
-    reverse_array(arr, n);
+    reverse_array(arr, 0, n-1);
     printf("Reversed array is \n");
     printArray(arr, n);   
     return 0;
